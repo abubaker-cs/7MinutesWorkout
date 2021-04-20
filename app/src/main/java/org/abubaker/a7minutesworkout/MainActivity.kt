80dcaf1fdf48160dcebcdf418d74b353bc151269
abubaker <mohammad_abubaker@yahoo.com>
2021-04-21 01:57:32 +0500

    Base setup

--- app/src/main/java/org/abubaker/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/org/abubaker/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,19 @@ package org.abubaker.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
     }
 }
