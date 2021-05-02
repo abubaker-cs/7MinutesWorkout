4e9fa0e9559f44db93b06328f584c0fc369c40f4
abubaker <mohammad_abubaker@yahoo.com>
2021-05-02 16:17:13 +0500

    Working on activity_exercise.xml

--- app/src/main/java/org/abubaker/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/org/abubaker/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.abubaker.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,14 +12,22 @@ class MainActivity : AppCompatActivity() {
     // Binding Object
     private lateinit var binding: ActivityMainBinding
 
-    //
+    // onCreate()
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
 
+        // Start Button = Toast
         binding.llStart.setOnClickListener {
-            Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+            /**
+             * ::class.java = getClass()
+             * To retrieve the Java Class of an object, use the JAVA extension property on a class reference
+             * Example: val fooClass = foo::class.java
+             */
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
 
     }
