189e29540d7494d5997c79442b69fd21991b2f25
abubaker <mohammad_abubaker@yahoo.com>
2021-05-02 16:58:45 +0500

    Working...

--- app/src/main/java/org/abubaker/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/org/abubaker/a7minutesworkout/ExerciseActivity.kt
@@ -2,10 +2,31 @@ package org.abubaker.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityExerciseBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercise)
+        binding = DataBindingUtil.setContentView(this@ExerciseActivity, R.layout.activity_exercise)
+
+        setSupportActionBar(binding.toolbarExerciseActivity)
+
+        val actionbar = supportActionBar
+
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true)
+        }
+
+        // Back Button in the Action Bar (TOP)
+        binding.toolbarExerciseActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
