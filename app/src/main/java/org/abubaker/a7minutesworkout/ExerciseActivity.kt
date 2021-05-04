726333566be6aa9a54429c39975e74b28d427fe9
abubaker <mohammad_abubaker@yahoo.com>
2021-05-04 20:17:29 +0500

    Working...

--- app/src/main/java/org/abubaker/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/org/abubaker/a7minutesworkout/ExerciseActivity.kt
@@ -2,6 +2,8 @@ package org.abubaker.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import org.abubaker.a7minutesworkout.databinding.ActivityExerciseBinding
 
@@ -10,6 +12,11 @@ class ExerciseActivity : AppCompatActivity() {
     // Binding Object
     private lateinit var binding: ActivityExerciseBinding
 
+    //
+    private var restTimer: CountDownTimer? = null
+
+    // It will just count up, instead of counting down. i.e. 0...10
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +35,58 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        //
+        setupRestView()
 
 
     }
+
+    private fun setRestProgressBar() {
+
+        // = 0
+        binding.progressBar.progress = restProgress
+
+        // 10sec - 1sec
+        restTimer = object : CountDownTimer(10000, 1000) {
+
+            // On every single tick
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+
+                // It will decrease values
+                binding.progressBar.progress = 10 - restProgress
+
+                binding.tvTimer.text = (10 - restProgress).toString()
+
+            }
+
+            // Once Finished | After 10 seconds
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+    }
+
+    /**
+     *
+     */
+    private fun setupRestView() {
+        if (restTimer != null) {
+
+            // rest it
+            restTimer!!.cancel()
+
+            // Put it back to zero
+            restProgress = 0
+
+            // initiate this function
+            setRestProgressBar()
+        }
+    }
+
 }
