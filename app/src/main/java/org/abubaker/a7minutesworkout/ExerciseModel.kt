45ea416063a3a87987d41a1e69aa5d5b6286cad0
abubaker <mohammad_abubaker@yahoo.com>
2021-05-05 04:46:33 +0500

    Working...

---
+++ app/src/main/java/org/abubaker/a7minutesworkout/ExerciseModel.kt
@@ -0,0 +1,13 @@
+package org.abubaker.a7minutesworkout
+
+class ExerciseModel(
+    private var id: Int,
+    private var name: String,
+    private var image: Int,
+    private var isCompleted: Boolean,
+    private var isSelected: Boolean
+) {
+
+
+
+}
