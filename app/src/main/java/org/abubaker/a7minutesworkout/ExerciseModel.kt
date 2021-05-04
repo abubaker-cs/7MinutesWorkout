1c30f734be59b1ea7448f68f2e37f5febd957692
abubaker <mohammad_abubaker@yahoo.com>
2021-05-05 04:51:14 +0500

    Working...

--- app/src/main/java/org/abubaker/a7minutesworkout/ExerciseModel.kt
+++ app/src/main/java/org/abubaker/a7minutesworkout/ExerciseModel.kt
@@ -8,6 +8,44 @@ class ExerciseModel(
     private var isSelected: Boolean
 ) {
 
+    fun getId(): Int {
+        return id
+    }
 
+    fun setId(id: Int) {
+        this.id = id
+    }
+
+    fun getName(): String {
+        return name
+    }
+
+    fun setName(name: String) {
+        this.name = name
+    }
+
+    fun getImage(): Int {
+        return image
+    }
+
+    fun setImage(image: Int) {
+        this.image = image
+    }
+
+    fun getIsCompleted(): Boolean {
+        return isCompleted
+    }
+
+    fun setIsCompleted(isCompleted: Boolean) {
+        this.isCompleted = isCompleted
+    }
+
+    fun getIsSelected(): Boolean {
+        return isSelected
+    }
+
+    fun setIsSelected(isSelected: Boolean) {
+        this.isSelected = isSelected
+    }
 
 }
