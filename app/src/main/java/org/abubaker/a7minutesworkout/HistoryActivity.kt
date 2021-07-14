599a963adb028e408449ef8a4c8397cfa38496a6
abubaker <mohammad_abubaker@yahoo.com>
2021-07-15 02:44:04 +0500

    Working on UI for the History Activity

---
+++ app/src/main/java/org/abubaker/a7minutesworkout/HistoryActivity.kt
@@ -0,0 +1,38 @@
+package org.abubaker.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.a7minutesworkout.databinding.ActivityHistoryBinding
+
+class HistoryActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityHistoryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this@HistoryActivity, R.layout.activity_history)
+
+        // Enable ActionBar using setSupportActionBar()
+        setSupportActionBar(binding.toolbarHistoryActivity)
+
+        // Assigning recently enabled ActionBar to the variable, using supportActionBar
+        val actionbar = supportActionBar
+
+        if (actionbar != null) {
+            // Enable back Button
+            actionbar.setDisplayHomeAsUpEnabled(true)
+
+            // Changing the default title
+            actionbar.title = "CALCULATE ME"
+        }
+
+        binding.toolbarHistoryActivity.setNavigationOnClickListener {
+            // Redirect to the HOME Screen
+            onBackPressed()
+        }
+
+    }
+
+}
