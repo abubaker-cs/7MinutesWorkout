5b7f88cab5a0f5c435f0cbb8eef4c8afe1ab8493
abubaker <mohammad_abubaker@yahoo.com>
2021-07-16 01:02:14 +0500

    Working...

--- app/src/main/java/org/abubaker/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/org/abubaker/a7minutesworkout/HistoryActivity.kt
@@ -2,8 +2,10 @@ package org.abubaker.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import org.abubaker.a7minutesworkout.databinding.ActivityHistoryBinding
+import org.abubaker.a7minutesworkout.db.SqliteOpenHelper
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -31,6 +33,27 @@ class HistoryActivity : AppCompatActivity() {
         binding.toolbarHistoryActivity.setNavigationOnClickListener {
             // Redirect to the HOME Screen
             onBackPressed()
+        }
+
+        // Calling a function for getting the list of completed dates when the History screen is launched
+        getAllCompletedDates()
+
+    }
+
+    /**
+     * Function is used to get the list exercise completed dates.
+     */
+    private fun getAllCompletedDates() {
+
+        // Instance of the SqliteOpenHelper.kt class
+        val dbHandler = SqliteOpenHelper(this, null)
+
+        // Get list of data from the history table
+        val allCompletedDatesList = dbHandler.getAllCompletedDatesList()
+
+        // List items are printed in log.
+        for (i in allCompletedDatesList) {
+            Log.e("Date : ", "" + i)
         }
 
     }
